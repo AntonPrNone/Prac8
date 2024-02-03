af0605281b57d5baea74ac009238ac82c7489cca
Антон <61877084+AntonPrNone@users.noreply.github.com>
2024-02-03 11:59:58 +0300

    Добавьте файлы проекта.

---
+++ Prac8/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Prac8
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Vector vector1 = new(1, 2, 3);
+            Vector vector2 = new(4, 5, 6);
+
+            Console.WriteLine("Original vectors:");
+            vector1.Print();
+            vector2.Print();
+
+            Vector sum = vector1.Add(vector2);
+            Console.WriteLine("\nSum vector:");
+            sum.Print();
+
+            Vector difference = vector1.Subtract(vector2);
+            Console.WriteLine("\nDifference vector:");
+            difference.Print();
+
+            Console.WriteLine("\nLength of vector1: " + vector1.Length);
+            Console.WriteLine("Length of sum: " + sum.Length);
+            Console.WriteLine("Length of difference: " + difference.Length);
+        }
+    }
+}
---
+++ Prac8/Vector.cs
@@ -0,0 +1,40 @@
+﻿class Vector
+{
+    // Поля
+    private readonly double x, y, z;
+
+    // Конструкторы
+    public Vector()
+    {
+        x = y = z = 0;
+    }
+
+    public Vector(double x, double y, double z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    // Свойства
+    public double Length
+    {
+        get { return Math.Sqrt(x * x + y * y + z * z); }
+    }
+
+    // Методы
+    public void Print()
+    {
+        Console.WriteLine($"({x}, {y}, {z})");
+    }
+
+    public Vector Add(Vector otherVector)
+    {
+        return new Vector(x + otherVector.x, y + otherVector.y, z + otherVector.z);
+    }
+
+    public Vector Subtract(Vector otherVector)
+    {
+        return new Vector(x - otherVector.x, y - otherVector.y, z - otherVector.z);
+    }
+}
